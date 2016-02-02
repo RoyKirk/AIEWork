9bcae535f60035e4706d73997d9df13ad0f706b8
Roy Kirk <ricotalzier@gmail.com>
2016-02-02 15:35:41 +1100

    solar system work

--- AIEOpenGL/src/TestApplication.cpp
+++ AIEOpenGL/src/TestApplication.cpp
@@ -13,24 +13,38 @@ using glm::vec3;
 using glm::vec4;
 using glm::mat4;
 
-float SunX = 0;
-float SunY = 0;
-float SunZ = 0;
-float Planet1X = 6;
-float Planet2X = 10;
-float Planet3X = 15;
-float Planet1Y = 0;
-float Planet2Y = 0;
-float Planet3Y = 0;
-float Planet1Z = 6;
-float Planet2Z = 10;
-float Planet3Z = 15;
-vec3 SunPos = vec3(SunX, SunY, SunZ);
-vec3 Planet1Pos = vec3(Planet1X, Planet1Y, Planet1Z);
-vec3 Planet2Pos = vec3(Planet2X, Planet2Y, Planet2Z);
-vec3 Planet3Pos = vec3(Planet3X, Planet3Y, Planet3Z);
+vec3 SunPos = vec3(0, 0, 0);
+vec3 Planet1Pos = vec3(6, 0, 6);
+vec3 Planet2Pos = vec3(10, 0, 10);
+vec3 Planet3Pos = vec3(15, 0, 15);
 
-mat4 Planet1Rot = mat4(cos(5.0), 0, sin(5.0), 0, 0, 1, 0, 0, -(sin(5.0)), 0, cos(5.0), 0, 0, 0, 0, 0);
+vec4 SunPos4 = vec4(SunPos.x, SunPos.y, SunPos.z, 1);
+vec4 Planet1Pos4 = vec4(Planet1Pos.x, Planet1Pos.y, Planet1Pos.z, 1);
+vec4 Planet2Pos4 = vec4(Planet2Pos.x, Planet2Pos.y, Planet2Pos.z, 1);
+vec4 Planet3Pos4 = vec4(Planet3Pos.x, Planet3Pos.y, Planet3Pos.z, 1);
+
+float Planet1OrbitRot = 0;
+float Planet2OrbitRot = 0;
+float Planet3OrbitRot = 0;
+
+float Planet1RotSpeed = 0.02;
+float Planet2RotSpeed = 0.01;
+float Planet3RotSpeed = 0.005;
+
+mat4 Planet1Rot = mat4(0, 0, 0, 0,
+						0, 1, 0, 0,
+						0, 0, 0, 0,
+						0, 0, 0, 1);
+
+mat4 Planet2Rot = mat4(cos(Planet2RotSpeed), 0, sin(Planet2RotSpeed), 0,
+	0, 1, 0, 0,
+	-(sin(Planet2RotSpeed)), 0, cos(Planet2RotSpeed), 0,
+	0, 0, 0, 1);
+
+mat4 Planet3Rot = mat4(cos(Planet3RotSpeed), 0, sin(Planet3RotSpeed), 0,
+	0, 1, 0, 0,
+	-(sin(Planet3RotSpeed)), 0, cos(Planet3RotSpeed), 0,
+	0, 0, 0, 1);
 
 
 TestApplication::TestApplication()
@@ -51,7 +65,7 @@ bool TestApplication::startup() {
 	Gizmos::create();
 
 	// create a camera
-	m_camera = new Camera(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
+	m_camera = new Camera(glm::pi<float>() * 0.25f, 16 / 9.f, 0.01f, 1000.f);
 	m_camera->setLookAtFrom(vec3(10, 10, 10), vec3(0));
 	
 	//////////////////////////////////////////////////////////////////////////
@@ -113,41 +127,26 @@ bool TestApplication::update(float deltaTime) {
 			i == 25 ? vec4(1, 1, 1, 1) : vec4(0, 0, 0, 1));
 	}
 	
-	vec4 SunPos4 = vec4(SunPos.x, SunPos.y, SunPos.z,0);
-	vec4 Planet1Pos4 = vec4(Planet1Pos.x, Planet1Pos.y, Planet1Pos.z,0);
-	vec4 Planet2Pos4 = vec4(Planet2Pos.x, Planet2Pos.y, Planet2Pos.z,0);
-	vec4 Planet3Pos4 = vec4(Planet3Pos.x, Planet3Pos.y, Planet3Pos.z,0);
+	Planet1OrbitRot += deltaTime * Planet1RotSpeed;
 
-	//Planet1Pos4 = Planet1Pos4 * Planet1Rot;
+	Planet1Rot = glm::rotate(Planet1OrbitRot, vec3(0, 1, 0));
+
+
+
+	Planet1Pos4 = Planet1Rot * Planet1Pos4;
+	//Planet2Pos4 = (Planet2Rot * Planet1Rot) * Planet2Pos4;
+	//Planet2Pos4 = Planet1Rot * Planet2Pos4;
+	//Planet3Pos4 = Planet3Rot * Planet3Pos4;
 	
-	vec3 SunPos = vec3(SunPos4.x, SunPos4.y, SunPos4.z);
-	vec3 Planet1Pos = vec3(Planet1Pos4.x, Planet1Pos4.y, Planet1Pos4.z);
-	vec3 Planet2Pos = vec3(Planet2Pos4.x, Planet2Pos4.y, Planet2Pos4.z);
-	vec3 Planet3Pos = vec3(Planet3Pos4.x, Planet3Pos4.y, Planet3Pos4.z);
+	SunPos = vec3(SunPos4.x, SunPos4.y, SunPos4.z);
+	Planet1Pos = vec3(Planet1Pos4.x, Planet1Pos4.y, Planet1Pos4.z);
+	Planet2Pos = vec3(Planet2Pos4.x, Planet2Pos4.y, Planet2Pos4.z);
+	Planet3Pos = vec3(Planet3Pos4.x, Planet3Pos4.y, Planet3Pos4.z);	
 
 	Gizmos::addSphere(SunPos, 3, 10, 10, vec4(1, 0, 1, 1));
 	Gizmos::addSphere(Planet1Pos, 0.5, 10, 10, vec4(1, 0, 0, 1));
 	Gizmos::addSphere(Planet2Pos, 1, 10, 10, vec4(0, 1, 0, 1));
 	Gizmos::addSphere(Planet3Pos, 1.5, 10, 10, vec4(0, 0, 1, 1));
-
-	//SunPos = SunPos;
-	//Planet1Pos = Planet1Pos;
-	//Planet2Pos = Planet2Pos;
-	//Planet3Pos = Planet3Pos;
-
-	//Planet1X = SunX + ((cos(4.0f)) * (Planet1X - SunX) + (sin(4.0f)) * (Planet1Z - SunZ));
-	//Planet1Z = SunZ + ((sin(4.0f)) * (Planet1X - SunX) + (cos(4.0f)) * (Planet1Z - SunZ));
-
-	//Planet2X = SunX + ((cos(4.0f)) * (Planet2X - SunX) + (sin(4.0f)) * (Planet2Z - SunZ));
-	//Planet2Z = SunZ + ((sin(4.0f)) * (Planet2X - SunX) + (cos(4.0f)) * (Planet2Z - SunZ));
-
-	//Planet3X = SunX + ((cos(4.0f)) * (Planet3X - SunX) + (sin(4.0f)) * (Planet3Z - SunZ));
-	//Planet3Z = SunZ + ((sin(4.0f)) * (Planet3X - SunX) + (cos(4.0f)) * (Planet3Z - SunZ));
-
-	//Planet1X = SunX + (Planet1X - SunX)*cos(1.0f) - (Planet1Z - SunZ)*sin(1.0f);
-	//Planet1Z = SunZ + (Planet1X - SunX)*sin(1.0f) + (Planet1Z - SunZ)*cos(1.0f);
-
-
 
 	// return true, else the application closes
 	return true;
