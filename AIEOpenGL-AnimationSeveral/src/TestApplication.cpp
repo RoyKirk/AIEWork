129f2d376c751ca8cbf7b9081866b7612e98da68
Roy Kirk <ricotalzier@gmail.com>
2016-03-07 00:15:51 +1100

    started culling

--- AIEOpenGL-AnimationSeveral/src/TestApplication.cpp
+++ AIEOpenGL-AnimationSeveral/src/TestApplication.cpp
@@ -40,6 +40,33 @@ FBXFile* m_fbx2;
 
 float m_timer;
 
+class BoundingSphere
+{
+public:
+	BoundingSphere() : centre(0), radius(0) {}
+	~BoundingSphere() {}
+
+	void fit(const std::vector<glm::vec3>& points) {
+		
+		glm::vec3 min(1e37f), max(-1e37f);
+		
+		for (auto& p : points)
+		{
+			if (p.x < min.x) { min.x = p.x; }
+			if (p.y < min.y) { min.y = p.y; }
+			if (p.z < min.z) { min.z = p.z; }
+			if (p.x > max.x) { max.x = p.x; }
+			if (p.y > max.y) { max.y = p.y; }
+			if (p.z > max.z) { max.z = p.z; }
+		}
+
+		centre = (min + max)*0.5f;
+		radius = glm::distance(min, centre);
+	}
+	glm::vec3 centre;
+	float radius;
+};
+
 void cleanupOpenGLBuffers(FBXFile* fbx)
 {
 	for (unsigned int i = 0; i < fbx->getMeshCount(); ++i)
@@ -56,7 +83,7 @@ void cleanupOpenGLBuffers(FBXFile* fbx)
 
 void createOpenGLBuffers(FBXFile* fbx)
 {
-	for (unsigned int i = 0; i < fbx->getMeshCount(); ++i)
+for (unsigned int i = 0; i < fbx->getMeshCount(); ++i)
 	{
 		FBXMeshNode* mesh = fbx->getMeshByIndex(i);
 		unsigned int* glData = new unsigned int[3];
